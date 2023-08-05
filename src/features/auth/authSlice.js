10e3964fe6518452ba94e01ba32bf98e04f0f063
shakhawatsalam <shakhawatsalam726@gmail.com>
2023-08-05 09:43:13 +0600

    [Firebase and Redux Setup is almost done]

---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  email: "",
+  role: "",
+  isLoading: true,
+  isError: false,
+  error: "",
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {},
+});
+
+export default authSlice.reducer;
